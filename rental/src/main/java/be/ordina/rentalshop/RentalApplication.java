65367b8048fadc4d32b2713a8e756d0107aa7238
Yannick De Turck <yannickdeturck@gmail.com>
2017-09-08 15:03:45 +0200

    Finish part 2

--- rental/src/main/java/be/ordina/rentalshop/RentalApplication.java
+++ rental/src/main/java/be/ordina/rentalshop/RentalApplication.java
@@ -2,7 +2,9 @@ package be.ordina.rentalshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class RentalApplication {
 
